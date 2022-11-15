0d83a94539c73b7c059edd05f0f01df6969be20c
irack000 <IRACK000@gmail.com>
2022-11-15 23:22:33 +0900

    Chore: Fix Indent

--- app/src/main/java/com/example/auction/app/ui/layout/UserMain.kt
+++ app/src/main/java/com/example/auction/app/ui/layout/UserMain.kt
@@ -3,19 +3,18 @@ package com.example.auction.app.ui.layout
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.auction.app.ui.shapes.PopularPreview
 import com.example.auction.app.ui.shapes.*
 
 @Composable
 fun UserMain() {
-        Column(
-            Modifier.fillMaxSize(),
-        ) {
-            TopAppBar()
-            NoticePreview()
-            Buttons()
-            PopularPreview()
-        }
+    Column(
+        Modifier.fillMaxSize()
+    ) {
+        TopAppBar()
+        NoticePreview()
+        Buttons()
+        PopularPreview()
+    }
 }
